eb6907b68cd22ff73377b9b591736d18a269e315
Lovanjatovo <hei.lovanjatovo@gmail.com>
2026-08-26 08:28:20 +0300

    feat(database): added the sql insertion for the options for the first question

---
+++ src/back/database/inserts.sql
@@ -0,0 +1,5 @@
+INSERT INTO questions (title) VALUES ('C'' est quoi react ?');
+INSERT INTO options (question_id , wording , is_correct) VALUES (1 , 'Une langage de programmation' , FALSE);
+INSERT INTO options (question_id , wording , is_correct) VALUES (1 , 'Une plateforme pour coder en live' , FALSE);
+INSERT INTO options (question_id , wording , is_correct) VALUES (1 , 'Une bibliotheque JavasCript' , TRUE);
+INSERT INTO options (question_id , wording , is_correct) VALUES (1 , 'Un effet de reaction pour une fonction' , FALSE);
